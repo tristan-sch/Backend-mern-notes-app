d98bcb723cd61fefbecedb43f05ef595bbce7ebc
Tristan <tristan@Tristans-iMac.local>
2022-01-10 15:47:03 +0000

    updated server.js

--- server.js
+++ server.js
@@ -14,6 +14,5 @@ app.use("/", require("./routes/noteRoute"));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.listen(8080, function () {
-  console.log("express server is running on port 8080");
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
