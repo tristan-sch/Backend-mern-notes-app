460b54f7e525b376d766272d833d67ae042cd91e
Tristan <tristan@Tristans-iMac.local>
2022-01-11 09:40:35 +0000

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // const express = require("express");
 // const app = express();
 // const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 //requiering express and initializing the app:
 const app = require("express")();
