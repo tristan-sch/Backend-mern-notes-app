5b0b8cd370fa49ba5d16bc3cee356fbe6fdc61d9
Tristan <tristan@Tristans-iMac.local>
2022-01-11 09:33:26 +0000

    udpated server.js

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
 const mongoose = require("mongoose");
 
+//requiering express and initializing the app:
+const app = require("express")();
+//requiering the cors middleware:
+const cors = require("cors");
+
+const PORT = 5001; //we will use port 5001
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://tristan:UcWtV4r9IljVDaCW@cluster0.tdpag.mongodb.net/notesDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 app.use(cors()); //telling express to use the cors middleware
-app.use(express.json());
-
-mongoose.connect(
-  "mongodb+srv://tristan:UcWtV4r9IljVDaCW@cluster0.tdpag.mongodb.net/notesDB?retryWrites=true&w=majority"
-);
-
 app.use("/", require("./routes/noteRoute"));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+app.listen(PORT, () => {
+  //listen to the port we chose above
+  //print to the console that the server is listening
+  console.log("listening to port: " + PORT);
+});
