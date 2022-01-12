7aaa1b1b171aa30ac2d7c5f28017826394abae5b
Tristan Schmale <tristanschmale@Tristans-iMac.local>
2022-01-12 11:07:10 +0000

    updated server.js

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-
-//requiering express and initializing the app:
-const app = require("express")();
-//requiering the cors middleware:
+const express = require("express");
+const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-const PORT = 5001; //we will use port 5001
+// //requiering express and initializing the app:
+// const app = require("express")();
+// //requiering the cors middleware:
+// const cors = require("cors");
 
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://tristan:UcWtV4r9IljVDaCW@cluster0.tdpag.mongodb.net/notesDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+// const PORT = 5001; //we will use port 5001
+
+mongoose.connect("mongodb+srv://tristan:UcWtV4r9IljVDaCW@cluster0.tdpag.mongodb.net/notesDB?retryWrites=true&w=majority")
+
+// const MongoClient = require("mongodb").MongoClient;
+// const uri =
+//   "mongodb+srv://tristan:UcWtV4r9IljVDaCW@cluster0.tdpag.mongodb.net/notesDB?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.use(cors()); //telling express to use the cors middleware
+app.use(express.json());
 app.use("/", require("./routes/noteRoute"));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(PORT, () => {
+app.listen(5001, () => {
   //listen to the port we chose above
   //print to the console that the server is listening
-  console.log("listening to port: " + PORT);
+  console.log("listening to port 5001");
 });
